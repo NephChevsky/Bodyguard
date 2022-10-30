0e36065dc46bc36a44b28e9e1ff370a1b22effee
Philippe Balleydier <philippe.balleydier@gmail.com>
2022-10-30 19:41:53 +0100

    add message analyzer

---
+++ TwitchAnalyzer/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using System.Reflection;
+using TwitchAnalyzer.Services;
+
+namespace TwitchAnalyzer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string projectName = Assembly.GetCallingAssembly().GetName().Name ?? "App";
+
+			IHost host = Host.CreateDefaultBuilder(args)
+			.ConfigureAppConfiguration((hostingContext, configBuilder) =>
+			{
+				System.IO.Directory.SetCurrentDirectory(System.AppDomain.CurrentDomain.BaseDirectory);
+				IConfigurationRoot config = configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("config.json", false)
+					.AddJsonFile("secret.json", false)
+					.Build();
+			})
+			.ConfigureServices(services =>
+			{
+				services.Configure<HostOptions>(hostOptions =>
+				{
+					hostOptions.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
+				});
+
+				services.AddHostedService<MessageAnalyzer>();
+
+				services.AddLogging(logging =>
+				{
+					logging.ClearProviders();
+					logging.AddNLog("nlog.config");
+					GlobalDiagnosticsContext.Set("appName", projectName);
+				});
+			})
+			.UseWindowsService()
+			.Build();
+
+			host.Run();
+		}
+	}
+}
---
+++ TwitchAnalyzer/Services/MessageAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using Db;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Models;
+using Models.Db;
+using NLog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchAnalyzer.Services
+{
+	internal class MessageAnalyzer : BackgroundService
+	{
+		private Settings _settings;
+		private readonly ILogger _logger;
+
+		public MessageAnalyzer()
+		{
+			_settings = new Settings().LoadSettings();
+			var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
+				.ClearProviders()
+				.AddNLog("nlog.config"));
+			_logger = loggerFactory.CreateLogger<MessageAnalyzer>();
+		}
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			while (!stoppingToken.IsCancellationRequested)
+			{
+				_logger.LogInformation("MessageAnalyzer running at: {time}", DateTimeOffset.Now);
+
+				using (BodyguardDbContext db = new())
+				{
+					List<TwitchMessage> messages = db.TwitchMessages.Where(x => x.Sentiment == false).ToList();
+					foreach (TwitchMessage message in messages)
+					{
+						// TODO: predict sentiment
+					}
+					db.SaveChanges();
+				}
+
+				await Task.Delay(60 * 1000, stoppingToken);
+			}
+		}
+	}
+}
